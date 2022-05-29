0c3fa679dec475a1b21ed0565e0112e5e0ec11a7
Priyal Saxena <priyalsaxena66@gmail.com>
2022-05-30 00:04:48 +0530

    more edits

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -83,7 +83,7 @@ export default function IndexPage() {
 					</p>
 				) : (
 					<div>
-						<BlogSection placeid={data1.recomLists.genRecom} />
+						<BlogSection placeid={data2.recomLists.genRecom} />
 					</div>
 				)}
 				{/* gen recom */}
